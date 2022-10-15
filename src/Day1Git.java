cdf42028b31fc0e567134dfc837a42e435886d0b
Sümeyye G <110695686+sumeyye8@users.noreply.github.com>
2022-10-15 05:18:14 +0300

    Update Day1Git.java

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -4,6 +4,7 @@ public class Day1Git {
         System.out.println("Hello Git 2. Deneme");
         System.out.println("Hello Git 3. Deneme");
         System.out.println("Hello Git 4. Deneme");
+        System.out.println("Hello Git 5. Deneme");
 
     }
 }
