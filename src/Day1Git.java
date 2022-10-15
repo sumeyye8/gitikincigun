80cd4dde64d925aa8841d3149c453d70cd29394a
Sümeyye Geçici <sumeyyegecici1998@gmail.com>
2022-10-15 05:16:58 +0300

    BVC

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -3,6 +3,7 @@ public class Day1Git {
         System.out.println("Hello Git 1. Deneme");
         System.out.println("Hello Git 2. Deneme");
         System.out.println("Hello Git 3. Deneme");
+        System.out.println("Hello Git 4. Deneme");
 
     }
 }
